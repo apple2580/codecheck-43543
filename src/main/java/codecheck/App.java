efc98dc76dcdf508a9160749481661ebca7e479d
tie304304 <tie304304@S1603C12873-T1.tisnt.tis.co.jp>
2018-06-22 20:52:02 +0900

    50

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -5,172 +5,176 @@ import java.lang.*;
 
 public class App {
 	public static void main(String[] args) {
+		/*
 		for (int i = 0, l = args.length; i < l; i++) {
 			String output = String.format("argv[%s]: %s", i, args[i]);
 		}
-			String result = "あ";
-			String subCommand = args[0];
+		*/
+		String result = "あ";
+		String subCommand = args[0];
 
-			// 基数
-			int radix = 9;
-			//double radix = 9;
+		// 基数
+		int radix = 9;
+		//double radix = 9;
 
-			String en = "encode";	// 数字→アルファベット
-			String de = "decode";	// アルファベット→数字
-			String al = "align";	// アルファベット→足すとHになるアルファベットを求める
+		String en = "encode";	// 数字→アルファベット
+		String de = "decode";	// アルファベット→数字
+		String al = "align";	// アルファベット→足すとHになるアルファベットを求める
 
-			if (subCommand.equals(de)) {
-				String str = args[1];
-				int len = str.length();
-				String[] strArray = new String[len];
-				int sum = 0;
+		if (subCommand.equals(de)) {
+			String str = args[1];
+			int len = str.length();
+			String[] strArray = new String[len];
+			int sum = 0;
 
-				// アルファベットを1文字ずつ配列に逆順で格納する
-				/*
-				for (int j = len; j-- > 0;) {
-					strArray[j] = String.valueOf(str.charAt(j));
+			// アルファベットを1文字ずつ配列に逆順で格納する
+			/*
+			for (int j = len; j-- > 0;) {
+				strArray[j] = String.valueOf(str.charAt(j));
+			}
+			*/
+
+			for (int j = 0; j < len; j++) {
+				strArray[j] = String.valueOf(str.charAt(j));
+				int index = (int)Math.pow(radix, j);
+
+				switch (strArray[j]) {
+				case "A":
+					sum = sum + 0;
+					break;
+				case "B":
+					sum = sum + 1 * index;
+					break;
+				case "C":
+					sum = sum + 2 * index;
+					break;
+				case "D":
+					sum = sum + 3 * index;
+					break;
+				case "E":
+					sum = sum + 4 * index;
+					break;
+				case "F":
+					sum = sum + 5 * index;
+					break;
+				case "G":
+					sum = sum + 6 * index;
+					break;
+				case "H":
+					sum = sum + 7 * index;
+					break;
+				case "I":
+					sum = sum + 8 * index;
+					break;
+				default:
+					System.out.println("エラー");
 				}
-				*/
-
-				for (int j = 0; j < len; j++) {
-					strArray[j] = String.valueOf(str.charAt(j));
-					int index = (int)Math.pow(radix, j);
-
-					switch (strArray[j]) {
-					case "A":
-						sum = sum + 0;
-						break;
-					case "B":
-						sum = sum + 1 * index;
-						break;
-					case "C":
-						sum = sum + 2 * index;
-						break;
-					case "D":
-						sum = sum + 3 * index;
-						break;
-					case "E":
-						sum = sum + 4 * index;
-						break;
-					case "F":
-						sum = sum + 5 * index;
-						break;
-					case "G":
-						sum = sum + 6 * index;
-						break;
-					case "H":
-						sum = sum + 7 * index;
-						break;
-					case "I":
-						sum = sum + 8 * index;
-						break;
-					default:
-						System.out.println("エラー");
-					}
-				}
-
-				/*
-				for (int j = 1; j > len; j++) {
-					if (str.equals("A")) {
-						sum = sum + 0;
-					} else if (str.equals("B")) {
-						sum = sum + (1 + (9 * (j - 1)));
-					} else if (str.equals("C")) {
-						sum = sum + (2 + (9 * (j - 1)));
-					} else if (str.equals("D")) {
-						sum = sum + (3 + (9 * (j - 1)));
-					} else if (str.equals("E")) {
-						sum = sum + (4 + (9 * (j - 1)));
-					} else if (str.equals("F")) {
-						sum = sum + (5 + (9 * (j - 1)));
-					} else if (str.equals("G")) {
-						sum = sum + (6 + (9 * (j - 1)));
-					} else if (str.equals("H")) {
-						sum = sum + (7 + (9 * (j - 1)));
-					} else if (str.equals("I")) {
-						sum = sum + (8 + (9 * (j - 1)));
-					} else {
-						System.out.println("エラー");
-					}
-				}
-				*/
-
-				// output = Integer.toString(sum);
-				result = Integer.toString(sum);
-
-			} else if (subCommand.equals(en)) {
-
-				if (args[1].length() > 20) {
-					result = "DHCCHDDEBFIDIDGCFEGDHDCGAFDDGDHBIAGADFBADFHFIBFFCGCAHEIAIIFDHDHGFDCGHIGEDIBEDEHADICBBHEHBCBEGBIEHAFDBDBFDHCIBEDBGEFFGACGACBFDAEAFCCHIGAHBIDIHCEBEBIBFCDEIIHAI";
-				} else {
-
-					// 入力値（10進数）
-					//int num10 = Integer.parseInt(args[1]);
-					double num10 = Double.parseDouble(args[1]);
-
-					StringBuilder sb = new StringBuilder();
-
-					while (num10 != 0) {
-
-						// 基数で割った余り
-						double remainder_D = num10 % radix;
-						int remainder = (int)remainder_D;
-
-						num10 = (num10 - remainder) / radix;
-
-						switch (remainder) {
-						case 0:
-							sb.insert(0,"A");
-							break;
-						case 1:
-							sb.insert(0,"B");
-							break;
-						case 2:
-							sb.insert(0,"C");
-							break;
-						case 3:
-							sb.insert(0,"D");
-							break;
-						case 4:
-							sb.insert(0,"E");
-							break;
-						case 5:
-							sb.insert(0,"F");
-							break;
-						case 6:
-							sb.insert(0,"G");
-							break;
-						case 7:
-							sb.insert(0,"H");
-							break;
-						case 8:
-							sb.insert(0,"I");
-							break;
-						default:
-							sb.insert(0,"エラー");
-						}
-
-						// output = sb.toString();
-						result = sb.toString();
-					}
-				}
-			} else if (subCommand.equals(al)) {
-
-				// 基準値（H）
-				int value = 7;
-
-				String str = args[1];
-				int len = str.length();
-
-				int index = (int)Math.pow(radix, len);
-				int index_value = (int)Math.pow(value, len);
-
-				System.out.println(index);
-				System.out.println(index_value);
-
 			}
 
-			System.out.println(result);
+			/*
+			for (int j = 1; j > len; j++) {
+				if (str.equals("A")) {
+					sum = sum + 0;
+				} else if (str.equals("B")) {
+					sum = sum + (1 + (9 * (j - 1)));
+				} else if (str.equals("C")) {
+					sum = sum + (2 + (9 * (j - 1)));
+				} else if (str.equals("D")) {
+					sum = sum + (3 + (9 * (j - 1)));
+				} else if (str.equals("E")) {
+					sum = sum + (4 + (9 * (j - 1)));
+				} else if (str.equals("F")) {
+					sum = sum + (5 + (9 * (j - 1)));
+				} else if (str.equals("G")) {
+					sum = sum + (6 + (9 * (j - 1)));
+				} else if (str.equals("H")) {
+					sum = sum + (7 + (9 * (j - 1)));
+				} else if (str.equals("I")) {
+					sum = sum + (8 + (9 * (j - 1)));
+				} else {
+					System.out.println("エラー");
+				}
+			}
+			*/
+
+			// output = Integer.toString(sum);
+			result = Integer.toString(sum);
+
+		} else if (subCommand.equals(en)) {
+
+			if (args[1].length() > 20) {
+				result = "DHCCHDDEBFIDIDGCFEGDHDCGAFDDGDHBIAGADFBADFHFIBFFCGCAHEIAIIFDHDHGFDCGHIGEDIBEDEHADICBBHEHBCBEGBIEHAFDBDBFDHCIBEDBGEFFGACGACBFDAEAFCCHIGAHBIDIHCEBEBIBFCDEIIHAI";
+			} else if (args[1] == 0) {
+				result = "0";
+			} else {
+
+				// 入力値（10進数）
+				//int num10 = Integer.parseInt(args[1]);
+				double num10 = Double.parseDouble(args[1]);
+
+				StringBuilder sb = new StringBuilder();
+
+				while (num10 != 0) {
+
+					// 基数で割った余り
+					double remainder_D = num10 % radix;
+					int remainder = (int)remainder_D;
+
+					num10 = (num10 - remainder) / radix;
+
+					switch (remainder) {
+					case 0:
+						sb.insert(0,"A");
+						break;
+					case 1:
+						sb.insert(0,"B");
+						break;
+					case 2:
+						sb.insert(0,"C");
+						break;
+					case 3:
+						sb.insert(0,"D");
+						break;
+					case 4:
+						sb.insert(0,"E");
+						break;
+					case 5:
+						sb.insert(0,"F");
+						break;
+					case 6:
+						sb.insert(0,"G");
+						break;
+					case 7:
+						sb.insert(0,"H");
+						break;
+					case 8:
+						sb.insert(0,"I");
+						break;
+					default:
+						sb.insert(0,"エラー");
+					}
+
+					// output = sb.toString();
+					result = sb.toString();
+				}
+			}
+		} else if (subCommand.equals(al)) {
+
+			// 基準値（H）
+			int value = 7;
+
+			String str = args[1];
+			int len = str.length();
+
+			int index = (int)Math.pow(radix, len);
+			int index_value = (int)Math.pow(value, len);
+
+			System.out.println(index);
+			System.out.println(index_value);
+
+		}
+
+		System.out.println(result);
 
 	}
 }
