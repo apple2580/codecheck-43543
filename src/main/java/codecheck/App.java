0a23cb644fd15bc3c9b295117ee97b5a0ef6418f
tie304304 <tie304304@S1603C12873-T1.tisnt.tis.co.jp>
2018-06-22 23:18:42 +0900

    999

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -6,7 +6,7 @@ import java.lang.*;
 public class App {
 	public static void main(String[] args) {
 
-		String result = "99999999999999999999999999";
+		String result = "";
 		String subCommand = args[0];
 
 		// 基数
